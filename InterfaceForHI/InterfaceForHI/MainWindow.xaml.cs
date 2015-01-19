e9a762bd31944d80abdfef93b0fe734afea79096
HilalNazli <nhilalozdemir@gmail.com>
2015-01-19 16:02:52 +0200

    All sign videos are added to the directory. Some design elements are changed to be loaded in the code.

--- InterfaceForHI/InterfaceForHI/MainWindow.xaml.cs
+++ InterfaceForHI/InterfaceForHI/MainWindow.xaml.cs
@@ -20,40 +20,22 @@ namespace InterfaceForHI
     /// </summary>
     public partial class MainWindow : Window
     {
+        double ratio = 0;
         Boolean isRepeated = false;
+        Boolean isSvAnswersVisible = false;
         System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
         double svAnswersHorizontalOffset = 0;
         public MainWindow()
         {
             InitializeComponent();
             mainWindow.WindowState = WindowState.Maximized;
-            //ResizeMode = System.Windows.ResizeMode.CanResize;
-            //SystemParameters.VirtualScreenWidth
-            //System.Windows.SystemParameters.PrimaryScreenWidth;
-            //SystemParameters.VirtualScreenHeight
-            //System.Windows.SystemParameters.PrimaryScreenHeight;
-            //double ratio = System.Windows.SystemParameters.PrimaryScreenWidth / mainWindow.Width;
-            System.Diagnostics.Debug.WriteLine("Width is: " + mainWindow.Width);
-            System.Diagnostics.Debug.WriteLine("Height is: " + mainWindow.Height);
-
-            System.Diagnostics.Debug.WriteLine("Row Height is: " + mainWindow.Row2.Height);
-            double ratio = System.Windows.SystemParameters.PrimaryScreenHeight/mainWindow.Height;
+            ratio = System.Windows.SystemParameters.PrimaryScreenHeight/mainWindow.Height;
             
             mainWindow.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
             mainWindow.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
-            restartVideo();
-
+            loadQuestion1();
             initializeSizes(ratio);
-
-            /*
-             *dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-             *dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
-             *dispatcherTimer.Start(); 
-             */
-
-
         }
-
         private void initializeSizes(double ratio)
         {
            
@@ -77,7 +59,6 @@ namespace InterfaceForHI
             //double widthOfspAnswers = 960;
             //double widthOfsvAnswers = 960;
             double heightOfsvAnswers = 350;
-            double widthOfgAnswer = 295;
 
             double heightOfI_Next = 240;
             double widthOfI_Next = 64;
@@ -111,10 +92,6 @@ namespace InterfaceForHI
            // mainWindow.spAnswers.Width = widthOfspAnswers * ratio;
             //mainWindow.svAnswers.Width = widthOfsvAnswers * ratio;
             mainWindow.svAnswers.Height = heightOfsvAnswers * ratio;
-            mainWindow.gAnswer1.Width = widthOfgAnswer * ratio;
-            mainWindow.gAnswer2.Width = widthOfgAnswer * ratio;
-            mainWindow.gAnswer3.Width = widthOfgAnswer * ratio;
-            mainWindow.gAnswer4.Width = widthOfgAnswer * ratio;
 
             mainWindow.I_Next.Height = heightOfI_Next * ratio;
             mainWindow.I_Next.Width = widthOfI_Next * ratio;
@@ -133,45 +110,74 @@ namespace InterfaceForHI
 
         }
 
+
+        private void initializeAnswerVideoProperties(object sender, RoutedEventArgs args) {
+            MediaElement me = (MediaElement)args.Source;
+            DockPanel.SetDock(me, Dock.Top);
+            me.Height = 200 * ratio;
+            me.LoadedBehavior = System.Windows.Controls.MediaState.Manual;
+            me.MediaEnded += new RoutedEventHandler(AnswerMediaEnded);
+            me.Stretch = Stretch.Uniform;
+
+        }
+        private void initializeAnswerTextProperties(object sender, RoutedEventArgs args)
+        {
+            TextBlock tb = (TextBlock)args.Source;
+            tb.FontSize = 24;
+            tb.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            //Calisiyor mu?
+            tb.Foreground = (System.Windows.Media.Brush)mainWindow.Resources["BlackPen"];
+        }
+
+        private void initializeAnswerGridProperties(object sender, RoutedEventArgs args)
+        {
+            Grid g = (Grid)args.Source;
+            Thickness margin = new System.Windows.Thickness();
+            margin.Left = 32 * ratio;
+            g.Margin = margin;
+            g.Width = 295 * ratio;
+        }
+        private void initializeAnswerDockPanelProperties(object sender, RoutedEventArgs args)
+        {
+            DockPanel dp = (DockPanel)args.Source;
+            dp.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+        }
+        private void AnswerMediaEnded(object sender, RoutedEventArgs args)
+        {
+            MediaElement me = (MediaElement)args.Source;
+            me.Position = System.TimeSpan.Zero;
+            me.Play();
+        }
+
         private void meMainVideo_MediaEnded(object sender, RoutedEventArgs e)
         {
             if (!isRepeated)
             {
                 mainWindow.svAnswers.Visibility = System.Windows.Visibility.Visible;
-                meBasAgrisiVideo.Play();
-                meBasimDonuyorVideo.Play();
-                meBogazimSistiVideo.Play();
-                meBaskaVideo.Play();
+                isSvAnswersVisible = true;
+                playAllChildren(spAnswers);
             }
            
             restartVideo();
         }
+        public static void playAllChildren(StackPanel stackPanel)
+        {
+
+            //System.Collections.Generic.List<MediaElement> meAnswers = new System.Collections.Generic.List<MediaElement>();
+
+            foreach(Grid g in stackPanel.Children){
+                //System.Diagnostics.Debug.WriteLine("Grid found");
+                DockPanel d = g.Children.OfType<DockPanel>().FirstOrDefault();
+                MediaElement me = d.Children.OfType<MediaElement>().FirstOrDefault();
+                me.Play();
+            }
+        }
 
         private void restartVideo()
         {
-            
-            meMainVideo.LoadedBehavior = MediaState.Manual;
-
             meMainVideo.Position = System.TimeSpan.Zero;
             meMainVideo.Play();
-        }
-
-        private void meBasAgrisiVideo_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            meBasAgrisiVideo.Position = System.TimeSpan.Zero;
-            meBasAgrisiVideo.Play();
-        }
-
-        private void meBasDonmesiVideo_MediaEnded(object sender, RoutedEventArgs e)
-        {
-             meBasimDonuyorVideo.Position = System.TimeSpan.Zero;
-             meBasimDonuyorVideo.Play();
-        }
-
-        private void meBogazimSistiVideo_MediaEnded(object sender, RoutedEventArgs e)
-        {
-             meBogazimSistiVideo.Position = System.TimeSpan.Zero;
-             meBogazimSistiVideo.Play();
         }
 
         private void I_Next_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -184,12 +190,6 @@ namespace InterfaceForHI
             svAnswers.LineLeft();
            
 
-        }
-
-        private void meBaskaVideo_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            meBaskaVideo.Position = System.TimeSpan.Zero;
-            meBaskaVideo.Play();
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
@@ -211,11 +211,94 @@ namespace InterfaceForHI
         private void svAnswers_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             //System.Diagnostics.Debug.WriteLine("Visible changed!!!");
-
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
-            dispatcherTimer.Start(); 
+            if (isSvAnswersVisible) { 
+                dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+                dispatcherTimer.Start();
+            }
         }
+
+
+
+
+
+
+
+
+
+
+
+        //Load Question1
+            //isRepeated=false;
+            //spAnswers.Children.Clear();
+        //Load AnswersToQuestion1
+        //Grid g = new Grid();
+        //MediaElement m = new MediaElement();
+        //m.Source = new System.Uri("SignVideos/BasAgrisi.mp4",UriKind.Relative);
+        //m.Loaded += new RoutedEventHandler(initializeAnswerProperties);
+        //g.Children.Add(m);
+        //spAnswers.Children.Add(g);
+        private void loadQuestion1() { 
+            //Set the question in the main video
+            meMainVideo.Source = new System.Uri("SignVideos/Questions/Question1/NasilYardimciOlabilirim.mp4",UriKind.Relative);
+            isRepeated = false;
+            restartVideo();
+            //Clear the stack panel for the answers
+            spAnswers.Children.Clear();
+            //Fill the stack panel with answers
+            //ANSWER1
+            //  Declare Answer1ToQuestion1 grid
+            Grid gAnswer1ToQuestion1 = new Grid();
+            spAnswers.Children.Add(gAnswer1ToQuestion1);
+            gAnswer1ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerGridProperties);
+            //    Declare Answer1ToQuestion1 dock
+            DockPanel dpAnswer1ToQuestion1 = new DockPanel();
+            gAnswer1ToQuestion1.Children.Add(dpAnswer1ToQuestion1);
+            dpAnswer1ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerDockPanelProperties);
+            //      Declare Answer1ToQuestion1 media element
+            MediaElement mAnswer1ToQuestion1 = new MediaElement();
+            dpAnswer1ToQuestion1.Children.Add(mAnswer1ToQuestion1);
+            mAnswer1ToQuestion1.Source = new System.Uri("SignVideos/Answers/AnswersToQuestion1/BenHastayim.mp4", UriKind.Relative);
+            mAnswer1ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerVideoProperties);
+            //      Declare Answer1ToQuestion1 text block
+            TextBlock tbAnswer1ToQuestion1 = new TextBlock();
+            dpAnswer1ToQuestion1.Children.Add(tbAnswer1ToQuestion1);
+            tbAnswer1ToQuestion1.Text = "Hastayım";
+            tbAnswer1ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerTextProperties);
+            //ANSWER2
+            //  Declare Answer2ToQuestion grid
+            Grid gAnswer2ToQuestion1 = new Grid();
+            spAnswers.Children.Add(gAnswer2ToQuestion1);
+            gAnswer2ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerGridProperties);
+            //    Declare Answer2ToQuestion1 dock
+            DockPanel dpAnswer2ToQuestion1 = new DockPanel();
+            gAnswer2ToQuestion1.Children.Add(dpAnswer2ToQuestion1);
+            dpAnswer2ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerDockPanelProperties);
+            //      Declare Answer2ToQuestion1 media element
+            MediaElement mAnswer2ToQuestion1 = new MediaElement();
+            dpAnswer2ToQuestion1.Children.Add(mAnswer2ToQuestion1);
+            mAnswer2ToQuestion1.Source = new System.Uri("SignVideos/Answers/AnswersToQuestion1/BilgiAlmakIstiyorum.mp4", UriKind.Relative);
+            mAnswer2ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerVideoProperties);
+            //      Declare Answer2ToQuestion1 text block
+            TextBlock tbAnswer2ToQuestion1 = new TextBlock();
+            dpAnswer2ToQuestion1.Children.Add(tbAnswer2ToQuestion1);
+            tbAnswer2ToQuestion1.Text = "Bilgi Almak İstiyorum";
+            tbAnswer2ToQuestion1.Loaded += new RoutedEventHandler(initializeAnswerTextProperties);
+        }
+
+
+
+
+
+        //Load Question2
+        //Load AnswersToQuestion2
+        //Load Question3
+        //Load AnswersToQuestion3
+        //Load Question4
+        //Load AnswersToQuestion4
+        //Load Direction1
+
+
 
     }
 }
